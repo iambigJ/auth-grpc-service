e4d39f87fe11a981644da9ae0c41f1a751617fa3
ALI.JAVADI <aliJavadiJ@gmail.com>
2023-08-13 14:50:58 +0330

    add user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,31 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { RedisModule } from './common/redis/redis.module';
 import { EmailModule } from './email/email.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, RedisModule, EmailModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres' as const,
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        migrations: ['../db/migrate'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
+    AuthModule,
+    RedisModule,
+    EmailModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
