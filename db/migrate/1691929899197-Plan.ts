3aa46a6f67fe2fc2889ed530e6cb8a40e80231f7
ALI.JAVADI <aliJavadiJ@gmail.com>
2023-08-14 14:20:18 +0330

    update migrates

--- db/migrate/1691929899197-Plan.ts
+++ db/migrate/1691929899197-Plan.ts
@@ -4,16 +4,15 @@ export class Plan1691929899197 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    CREATE TABLE "plan" (
-      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-      "name" character varying NOT NULL,
-      "data" jsonb NOT NULL DEFAULT '{}',
-      "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
-      "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
-      CONSTRAINT "UQ_name" UNIQUE ("name"),
-      PRIMARY KEY ("id")
-      )
-    `);
+    create table if not exists "Plans"
+    (
+        id          uuid not null primary key DEFAULT uuid_generate_v4(),
+        name        varchar(255)  not null unique,
+        data        jsonb     not null,
+        "createdAt" timestamp not null,
+        "updatedAt" timestamp,
+        "deletedAt" timestamp
+    );`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- db/migrate/1691930094047-User.ts
+++ db/migrate/1691930094047-User.ts
@@ -3,23 +3,22 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class User1691930094047 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        CREATE TABLE "user" (
-          "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-          "emailVerified" boolean NOT NULL,
-          "referralCode" character varying NOT NULL,
-          "payer_id" bigint NOT NULL,
-          "plan_id" uuid,
-          "mobile" character varying NOT NULL,
-          "role" character varying NOT NULL,
-          "password" character varying NOT NULL,
-          "profile" jsonb NOT NULL DEFAULT '{}',
-          "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
-          "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
-          PRIMARY KEY ("id")
-        );
-        ALTER TABLE "user" ADD CONSTRAINT "fk_user_plan" 
-        FOREIGN KEY ("plan_id") REFERENCES "plan"("id");
-        CREATE INDEX "idx_user_plan_id" ON "user" ("plan_id");
+      create table if not exists "Users"
+      (
+          id              uuid not null primary key DEFAULT uuid_generate_v4(),
+          payer_id        bigserial unique,
+          plan_id         uuid references "Plans" on delete set null,
+          mobile          varchar(255) unique constraint "Users_mobile_key1" unique,
+          role            varchar(255)                                     not null,
+          password        text,
+          profile         jsonb,
+          "createdAt"     timestamp not null,
+          "updatedAt"     timestamp,
+          "deletedAt"     timestamp,
+          email           varchar(255) unique,
+          "emailVerified" boolean,
+          "referralCode"  text default substr(md5((random())::text), 1, 8) not null unique
+      );
       `);
   }
 
